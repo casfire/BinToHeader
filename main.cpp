151c57463a9f7bedb0aa71a935e47dfebf0c5d40
casfire <caspignis@gmail.com>
2013-05-12 10:56:08 +0200

    bugfixes

--- main.cpp
+++ main.cpp
@@ -31,20 +31,24 @@ int main(int argc, char *argv[]){
 
 	//Input file name to upper case
 	char nameUpper[256];
-	strcpy(nameUpper, name);
+	sprintf(nameUpper, "_%s", name);
 	for(tmp = nameUpper; *tmp != 0; tmp++){
-		if(*tmp == '.')*tmp = '_';
-		else *tmp = toupper(*tmp);
+		if(((*tmp >= 'a')&&(*tmp <= 'z'))||((*tmp >= 'A')&&(*tmp <= 'Z'))||((*tmp >= '0')&&(*tmp <= '9'))){
+			*tmp = toupper(*tmp);
+		}else *tmp = '_';
 	}
 
 	//Input file name to lower case
 	char nameLower[256];
-	strcpy(nameLower, name);
+	if((*name >= '0')&&(*name <= '9'))sprintf(nameLower, "_%s", name);
+	else sprintf(nameLower, "%s", name);
 	for(tmp = nameLower; *tmp != 0; tmp++){
-		if(*tmp == '.')*tmp = '_';
-		else *tmp = tolower(*tmp);
+		if(((*tmp >= 'a')&&(*tmp <= 'z'))||((*tmp >= 'A')&&(*tmp <= 'Z'))||((*tmp >= '0')&&(*tmp <= '9'))){
+			*tmp = tolower(*tmp);
+		}else *tmp = '_';
 	}
 
+	//Open files
 	FILE *fi = fopen(argFile,"rb");
 	if(fi == NULL){
 		printf("Unable to open %s for reading.\n", argFile);
@@ -59,6 +63,7 @@ int main(int argc, char *argv[]){
 		return -1;
 	}
 
+	//Write output
 	fprintf(fo, "#ifndef %s_H_\n", nameUpper);
 	fprintf(fo, "#define %s_H_\n", nameUpper);
 	fprintf(fo, "\n");
@@ -66,7 +71,7 @@ int main(int argc, char *argv[]){
 	int c, count = 0;
 	while((c = fgetc(fi)) != EOF){
 		char ch = (unsigned char)c;
-		if(count == 11){
+		if(count == 12){
 			fprintf(fo, ",\n");
 			count = 0;
 		}
